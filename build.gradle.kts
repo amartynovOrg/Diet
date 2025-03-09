bafba133a72dc544421c5f1957522ed08e328b0b
Andrey <ivanovandreyodessa@gmail.com>
2025-03-09 20:42:38 +0200

    fix dependencies, update project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,13 +15,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
--- src/main/java/org/ivanov/andrey/diet/controller/Controller.java
+++ src/main/java/org/ivanov/andrey/diet/controller/Controller.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.ivanov.andrey.diet.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
